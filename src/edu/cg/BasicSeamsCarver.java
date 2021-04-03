73bcec7d7b58a1852e944ce27f211ead4c1403fd
Nadav Zaltsman <nzaltsman@bigid.com>
2021-04-03 19:34:32 +0300

    nadav commit

--- src/edu/cg/BasicSeamsCarver.java
+++ src/edu/cg/BasicSeamsCarver.java
@@ -5,220 +5,233 @@ import java.awt.image.BufferedImage;
 
 
 public class BasicSeamsCarver extends ImageProcessor {
-	
-	// An enum describing the carving scheme used by the seams carver.
-	// VERTICAL_HORIZONTAL means vertical seams are removed first.
-	// HORIZONTAL_VERTICAL means horizontal seams are removed first.
-	// INTERMITTENT means seams are removed intermittently : vertical, horizontal, vertical, horizontal etc.
-	public static enum CarvingScheme {
-		VERTICAL_HORIZONTAL("Vertical seams first"),
-		HORIZONTAL_VERTICAL("Horizontal seams first"),
-		INTERMITTENT("Intermittent carving");
-		
-		public final String description;
-		
-		private CarvingScheme(String description) {
-			this.description = description;
-		}
-	}
-	
-	// A simple coordinate class which assists the implementation.
-	protected class Coordinate{
-		public int X;
-		public int Y;
-		public Coordinate(int X, int Y) {
-			this.X = X;
-			this.Y = Y;
-		}
-	}
 
-	public static enum Mode {
-		HORIZONTAL("horizontal"),
-		VERTICAL("vertical");
+    // An enum describing the carving scheme used by the seams carver.
+    // VERTICAL_HORIZONTAL means vertical seams are removed first.
+    // HORIZONTAL_VERTICAL means horizontal seams are removed first.
+    // INTERMITTENT means seams are removed intermittently : vertical, horizontal, vertical, horizontal etc.
+    public static enum CarvingScheme {
+        VERTICAL_HORIZONTAL("Vertical seams first"),
+        HORIZONTAL_VERTICAL("Horizontal seams first"),
+        INTERMITTENT("Intermittent carving");
 
-		 public final String mode;
+        public final String description;
 
-		private Mode(String mode) {
-			this.mode = mode;
-		}
-	}
-	
-	// TODO :  Decide on the fields your BasicSeamsCarver should include. Refer to the recitation and homework 
-	private Coordinate[][] coordinates;
-	private double [][] dynamicPTable;
-	private double[][] energyMatrix;
-	private int[][] grayScaledMatrix;
-	private int currHeight;
-	private int currWidth;
-	private Mode currentMode;
-	
-	public BasicSeamsCarver(Logger logger, BufferedImage workingImage,
-			int outWidth, int outHeight, RGBWeights rgbWeights) {
-		super((s) -> logger.log("Seam carving: " + s), workingImage, rgbWeights, outWidth, outHeight);
-		currHeight = inHeight;
-		currWidth = inWidth;
-		coordinates = new Coordinate[currHeight][currWidth];
+        private CarvingScheme(String description) {
+            this.description = description;
+        }
+    }
 
-		grayScaledMatrix = new int[currHeight][currWidth];
-		setForEachParameters(currWidth, currHeight);
-		BufferedImage grayScaledImage = greyscale();
+    // A simple coordinate class which assists the implementation.
+    protected class Coordinate {
+        public int X;
+        public int Y;
 
-		forEach((y, x) -> {
-			coordinates[y][x] = new Coordinate(x,y);
-			grayScaledMatrix[y][x] = (new Color(grayScaledImage.getRGB(x, y))).getRed();
-		});
-	}
+        public Coordinate(int X, int Y) {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
 
-	private void updateDynamicPTableWithCostPixel() {
-		forEach((y, x) -> {
-			calcMinimalCostPixel(y, x);
-		});
-	}
+    public static enum Mode {
+        HORIZONTAL("horizontal"),
+        VERTICAL("vertical");
 
-	private void calcMinimalCostPixel(int y, int x) {
-		if (currentMode == Mode.VERTICAL) {
-			calcVerticalCost(y, x);
-		} else {
-			calcHorizontalCost(y, x);
-		}
-	}
+        public final String mode;
 
-	private void calcHorizontalCost(int y, int x) {
-		if (x == 0) {
-			if (y == 0 || y == this.currHeight -1) {
-				return;
-			} else {
-				this.dynamicPTable[y][x]+=  Math.abs(grayScaledMatrix[y - 1][x] - grayScaledMatrix[y + 1][x]);
-			}
-		} else {
-			this.dynamicPTable[y][x]+= calcHorizontalMinHorizontalValue(y, x);
-		}
-	}
+        private Mode(String mode) {
+            this.mode = mode;
+        }
+    }
 
-	private double calcHorizontalMinHorizontalValue(int y, int x) {
-		double mu;
-		double mh;
-		double md;
+    // TODO :  Decide on the fields your BasicSeamsCarver should include. Refer to the recitation and homework 
+    private Coordinate[][] coordinates;
+    private double[][] dynamicPTable;
+    private double[][] energyMatrix;
+    private int[][] grayScaledMatrix;
+    private int currHeight;
+    private int currWidth;
+    private Mode currentMode;
 
-		if (y == 0) {
-			mu = 255;
-			mh = dynamicPTable[y][x-1];
-			md = Math.abs(this.grayScaledMatrix[y+1][x] - this.grayScaledMatrix[y][x-1]) + dynamicPTable[y+1][x-1];
-		} else if (y == currHeight -1) {
-			md = 255;
-			mh = dynamicPTable[y][x-1];
-			mu = Math.abs(this.grayScaledMatrix[y - 1][x] - this.grayScaledMatrix[y][x-1]) + dynamicPTable[y-1][x-1];
-		} else {
-			md = Math.abs(this.grayScaledMatrix[y+1][x] - this.grayScaledMatrix[y][x-1])
-					+ Math.abs(grayScaledMatrix[y-1][x] - grayScaledMatrix[y+1][x])
-					+ dynamicPTable[y+1][x-1];
-			mu = Math.abs(this.grayScaledMatrix[y-1][x] - this.grayScaledMatrix[y][x-1])
-					+ Math.abs(grayScaledMatrix[y-1][x] - grayScaledMatrix[y+1][x])
-					+ dynamicPTable[y-1][x-1];
-			mh = Math.abs(this.grayScaledMatrix[y-1][x] - this.grayScaledMatrix[y+1][x]) + dynamicPTable[y][x-1];
-		}
 
-		return Math.min(mh, Math.min(mu, md));
-	}
+    public BasicSeamsCarver(Logger logger, BufferedImage workingImage,
+                            int outWidth, int outHeight, RGBWeights rgbWeights) {
+        super((s) -> logger.log("Seam carving: " + s), workingImage, rgbWeights, outWidth, outHeight);
+        currHeight = inHeight;
+        currWidth = inWidth;
+        coordinates = new Coordinate[currHeight][currWidth];
 
-	private void calcVerticalCost(int y, int x) {
-		if (y == 0) {
-			if (x == 0 || x == this.currWidth -1) {
-				return;
-			} else {
-				this.dynamicPTable[y][x]+=  Math.abs(grayScaledMatrix[y][x-1] - grayScaledMatrix[y][x+1]);
-			}
-		} else {
-			this.dynamicPTable[y][x]+= calcVerticalMinVerticalValue(y, x);
-		}
-	}
+        grayScaledMatrix = new int[currHeight][currWidth];
+        setForEachParameters(currWidth, currHeight);
+        BufferedImage grayScaledImage = greyscale();
 
-	private double calcVerticalMinVerticalValue(int y, int x) {
-		double ml;
-		double mv;
-		double mr;
+        forEach((y, x) -> {
+            coordinates[y][x] = new Coordinate(x, y);
+            grayScaledMatrix[y][x] = (new Color(grayScaledImage.getRGB(x, y))).getRed();
+        });
+    }
 
-		if (x == 0) {
-			ml = 255;
-			mv = dynamicPTable[y-1][x];
-			mr = Math.abs(this.grayScaledMatrix[y][x+1] - this.grayScaledMatrix[y - 1][x]) + dynamicPTable[y-1][x+1];
-		} else if (x == currWidth -1) {
-			mr = 255;
-			mv = dynamicPTable[y-1][x];
-			ml = Math.abs(this.grayScaledMatrix[y - 1][x] - this.grayScaledMatrix[y][x-1]) + dynamicPTable[y-1][x-1];
-		} else {
-			mr = Math.abs(this.grayScaledMatrix[y][x+1] - this.grayScaledMatrix[y][x-1])
-					+ Math.abs(grayScaledMatrix[y][x+1] - grayScaledMatrix[y-1][x])
-					+ dynamicPTable[y-1][x+1];
-			ml = Math.abs(this.grayScaledMatrix[y][x + 1] - this.grayScaledMatrix[y][x-1])
-					+ Math.abs(grayScaledMatrix[y-1][x] - grayScaledMatrix[y][x-1])
-					+ dynamicPTable[y-1][x-1];
-			mv = Math.abs(this.grayScaledMatrix[y][x + 1] - this.grayScaledMatrix[y][x - 1]) + dynamicPTable[y-1][x];
-		}
+    private void carveSeam() {
+        initDynamicPTableWithEnergyPixels();
+        forEach((y, x) -> {
+            calcMinimalCostPixel(y, x);
+        });
+    }
 
-		return Math.min(mv, Math.min(ml, mr));
-	}
+    private void calcMinimalCostPixel(int y, int x) {
+        if (currentMode == Mode.VERTICAL) {
+            calcVerticalCost(y, x);
+        } else {
+            calcHorizontalCost(y, x);
+        }
+    }
 
-	private void initDynamicPTableWithEnergyPixels() {
-		forEach((y,x) -> {
-			this.dynamicPTable[y][x] = calcEnergyPixel(x, y);
-		});
-	}
+    private void calcHorizontalCost(int y, int x) {
+        if (x == 0) {
+            if (y == 0 || y == this.currHeight - 1) {
+                return;
+            } else {
+                this.dynamicPTable[y][x] += Math.abs(grayScaledMatrix[y - 1][x] - grayScaledMatrix[y + 1][x]);
+            }
+        } else {
+            this.dynamicPTable[y][x] += calcHorizontalMinHorizontalValue(y, x);
+        }
+    }
 
-	private double calcEnergyPixel(int x, int y) {
-		double horizontalEnergy = 0;
-		double verticalEnergy = 0;
-		if(x == currWidth - 1){
-			horizontalEnergy = Math.abs(grayScaledMatrix[y][x] - grayScaledMatrix[y][x - 1]);
-		}else{
-			horizontalEnergy = Math.abs(grayScaledMatrix[y][x] - grayScaledMatrix[y][x + 1]);
-		}
+    private double calcHorizontalMinHorizontalValue(int y, int x) {
+        double mu;
+        double mh;
+        double md;
 
-		if(y == currHeight -1){
-			verticalEnergy = Math.abs(grayScaledMatrix[y][x] - grayScaledMatrix[y - 1][x]);
-		}else{
-			verticalEnergy = Math.abs(grayScaledMatrix[y][x] - grayScaledMatrix[y + 1][x]);
-		}
+        if (y == 0) {
+            mu = 255;
+            mh = dynamicPTable[y][x - 1];
+            md = Math.abs(this.grayScaledMatrix[y + 1][x] - this.grayScaledMatrix[y][x - 1]) + dynamicPTable[y + 1][x - 1];
+        } else if (y == currHeight - 1) {
+            md = 255;
+            mh = dynamicPTable[y][x - 1];
+            mu = Math.abs(this.grayScaledMatrix[y - 1][x] - this.grayScaledMatrix[y][x - 1]) + dynamicPTable[y - 1][x - 1];
+        } else {
+            md = Math.abs(this.grayScaledMatrix[y + 1][x] - this.grayScaledMatrix[y][x - 1])
+                    + Math.abs(grayScaledMatrix[y - 1][x] - grayScaledMatrix[y + 1][x])
+                    + dynamicPTable[y + 1][x - 1];
+            mu = Math.abs(this.grayScaledMatrix[y - 1][x] - this.grayScaledMatrix[y][x - 1])
+                    + Math.abs(grayScaledMatrix[y - 1][x] - grayScaledMatrix[y + 1][x])
+                    + dynamicPTable[y - 1][x - 1];
+            mh = Math.abs(this.grayScaledMatrix[y - 1][x] - this.grayScaledMatrix[y + 1][x]) + dynamicPTable[y][x - 1];
+        }
 
-		double ans = Math.sqrt(Math.pow(horizontalEnergy, 2) + Math.pow(verticalEnergy, 2));
-		return ans;
-	}
-	
-	public BufferedImage carveImage(CarvingScheme carvingScheme) {
-		int numberOfVerticalSeamsToCarve = Math.abs(this.outWidth - this.inWidth);
-		int numberOfHorizontalSeamsToCarve = Math.abs(this.outHeight - this.inHeight);
-		// TODO :  Perform Seam Carving. Overall you need to remove 'numberOfVerticalSeamsToCarve' vertical seams
-				// and 'numberOfHorizontalSeamsToCarve' horizontal seams from the image.
-				// Note you must consider the 'carvingScheme' parameter in your procedure.
-				// Return the resulting image.
-		initDynamicPTableWithEnergyPixels();
+        return Math.min(mh, Math.min(mu, md));
+    }
 
-		if (carvingScheme == CarvingScheme.VERTICAL_HORIZONTAL) {
-			this.currentMode = Mode.VERTICAL;
-			updateDynamicPTableWithCostPixel();
-			this.currentMode = Mode.HORIZONTAL;
-			updateDynamicPTableWithCostPixel();
-		} else if (carvingScheme == CarvingScheme.HORIZONTAL_VERTICAL) {
-			updateDynamicPTableWithCostPixel(CarvingScheme.HORIZONTAL_VERTICAL);
-		} else {
-			updateDynamicPTableWithCostPixel(CarvingScheme.INTERMITTENT);
-		}
-		throw new UnimplementedMethodException("carveImage");
-	}
-	
-	public BufferedImage showSeams(boolean showVerticalSeams, int seamColorRGB) {
-		int numberOfVerticalSeamsToCarve = Math.abs(this.outWidth - this.inWidth);
-		int numberOfHorizontalSeamsToCarve = Math.abs(this.outHeight - this.inHeight);
-		// TODO :  Present either vertical or horizontal seams on the input image.
-				// If showVerticalSeams = true, carve 'numberOfVerticalSeamsToCarve' vertical seams from the image.
-				// Then, generate a new image from the input image in which you mark all of the vertical seams that
-				// were chosen in the Seam Carving process. 
-				// This is done by painting each pixel in each seam with 'seamColorRGB' (overriding its' previous value). 
-				// Similarly, if showVerticalSeams = false, carve 'numberOfHorizontalSeamsToCarve' horizontal seams
-				// from the image.
-				// Then, generate a new image from the input image in which you mark all of the horizontal seams that
-				// were chosen in the Seam Carving process.
+    private void calcVerticalCost(int y, int x) {
+        if (y == 0) {
+            if (x == 0 || x == this.currWidth - 1) {
+                return;
+            } else {
+                this.dynamicPTable[y][x] += Math.abs(grayScaledMatrix[y][x - 1] - grayScaledMatrix[y][x + 1]);
+            }
+        } else {
+            this.dynamicPTable[y][x] += calcVerticalMinVerticalValue(y, x);
+        }
+    }
 
-		throw new UnimplementedMethodException("showSeams");
-	}
+    private double calcVerticalMinVerticalValue(int y, int x) {
+        double ml;
+        double mv;
+        double mr;
+
+        if (x == 0) {
+            ml = 255;
+            mv = dynamicPTable[y - 1][x];
+            mr = Math.abs(this.grayScaledMatrix[y][x + 1] - this.grayScaledMatrix[y - 1][x]) + dynamicPTable[y - 1][x + 1];
+        } else if (x == currWidth - 1) {
+            mr = 255;
+            mv = dynamicPTable[y - 1][x];
+            ml = Math.abs(this.grayScaledMatrix[y - 1][x] - this.grayScaledMatrix[y][x - 1]) + dynamicPTable[y - 1][x - 1];
+        } else {
+            mr = Math.abs(this.grayScaledMatrix[y][x + 1] - this.grayScaledMatrix[y][x - 1])
+                    + Math.abs(grayScaledMatrix[y][x + 1] - grayScaledMatrix[y - 1][x])
+                    + dynamicPTable[y - 1][x + 1];
+            ml = Math.abs(this.grayScaledMatrix[y][x + 1] - this.grayScaledMatrix[y][x - 1])
+                    + Math.abs(grayScaledMatrix[y - 1][x] - grayScaledMatrix[y][x - 1])
+                    + dynamicPTable[y - 1][x - 1];
+            mv = Math.abs(this.grayScaledMatrix[y][x + 1] - this.grayScaledMatrix[y][x - 1]) + dynamicPTable[y - 1][x];
+        }
+
+        return Math.min(mv, Math.min(ml, mr));
+    }
+
+    private void initDynamicPTableWithEnergyPixels() {
+        forEach((y, x) -> {
+            this.dynamicPTable[y][x] = calcEnergyPixel(x, y);
+        });
+    }
+
+    private double calcEnergyPixel(int x, int y) {
+        double horizontalEnergy = 0;
+        double verticalEnergy = 0;
+        if (x == currWidth - 1) {
+            horizontalEnergy = Math.abs(grayScaledMatrix[y][x] - grayScaledMatrix[y][x - 1]);
+        } else {
+            horizontalEnergy = Math.abs(grayScaledMatrix[y][x] - grayScaledMatrix[y][x + 1]);
+        }
+
+        if (y == currHeight - 1) {
+            verticalEnergy = Math.abs(grayScaledMatrix[y][x] - grayScaledMatrix[y - 1][x]);
+        } else {
+            verticalEnergy = Math.abs(grayScaledMatrix[y][x] - grayScaledMatrix[y + 1][x]);
+        }
+
+        double ans = Math.sqrt(Math.pow(horizontalEnergy, 2) + Math.pow(verticalEnergy, 2));
+        return ans;
+    }
+
+    public BufferedImage carveImage(CarvingScheme carvingScheme) {
+        int numberOfVerticalSeamsToCarve = Math.abs(this.outWidth - this.inWidth);
+        int numberOfHorizontalSeamsToCarve = Math.abs(this.outHeight - this.inHeight);
+
+        if (carvingScheme == CarvingScheme.VERTICAL_HORIZONTAL) {
+            this.currentMode = Mode.VERTICAL;
+            for (int i = 0; i < numberOfVerticalSeamsToCarve; i++) {
+                carveSeam();
+            }
+            this.currentMode = Mode.HORIZONTAL;
+            for (int i = 0; i < numberOfHorizontalSeamsToCarve; i++) {
+                carveSeam();
+            }
+        } else if (carvingScheme == CarvingScheme.HORIZONTAL_VERTICAL) {
+            this.currentMode = Mode.HORIZONTAL;
+            for (int i = 0; i < numberOfHorizontalSeamsToCarve; i++) {
+                carveSeam();
+            }
+
+            this.currentMode = Mode.VERTICAL;
+            for (int i = 0; i < numberOfVerticalSeamsToCarve; i++) {
+                carveSeam();
+            }
+        } else {
+            carveIntermittently();
+        }
+        throw new UnimplementedMethodException("carveImage");
+    }
+
+    private void carveIntermittently() {
+    }
+
+    public BufferedImage showSeams(boolean showVerticalSeams, int seamColorRGB) {
+        int numberOfVerticalSeamsToCarve = Math.abs(this.outWidth - this.inWidth);
+        int numberOfHorizontalSeamsToCarve = Math.abs(this.outHeight - this.inHeight);
+        // TODO :  Present either vertical or horizontal seams on the input image.
+        // If showVerticalSeams = true, carve 'numberOfVerticalSeamsToCarve' vertical seams from the image.
+        // Then, generate a new image from the input image in which you mark all of the vertical seams that
+        // were chosen in the Seam Carving process. 
+        // This is done by painting each pixel in each seam with 'seamColorRGB' (overriding its' previous value). 
+        // Similarly, if showVerticalSeams = false, carve 'numberOfHorizontalSeamsToCarve' horizontal seams
+        // from the image.
+        // Then, generate a new image from the input image in which you mark all of the horizontal seams that
+        // were chosen in the Seam Carving process.
+
+        throw new UnimplementedMethodException("showSeams");
+    }
 }
