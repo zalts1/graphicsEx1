7e84e39946114e1cf0bbbf19074495fef78a0f10
Nadav Zaltsman <nzaltsman@bigid.com>
2021-04-03 19:19:18 +0300

    nadav commit

--- src/edu/cg/BasicSeamsCarver.java
+++ src/edu/cg/BasicSeamsCarver.java
@@ -1,5 +1,6 @@
 package edu.cg;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 
@@ -30,46 +31,153 @@ public class BasicSeamsCarver extends ImageProcessor {
 			this.Y = Y;
 		}
 	}
+
+	public static enum Mode {
+		HORIZONTAL("horizontal"),
+		VERTICAL("vertical");
+
+		 public final String mode;
+
+		private Mode(String mode) {
+			this.mode = mode;
+		}
+	}
 	
 	// TODO :  Decide on the fields your BasicSeamsCarver should include. Refer to the recitation and homework 
 	private Coordinate[][] coordinates;
-	private BufferedImage grayScaledImage;
+	private double [][] dynamicPTable;
 	private double[][] energyMatrix;
-	private int[][] grayScaled;
+	private int[][] grayScaledMatrix;
+	private int currHeight;
+	private int currWidth;
+	private Mode currentMode;
 	
 	public BasicSeamsCarver(Logger logger, BufferedImage workingImage,
 			int outWidth, int outHeight, RGBWeights rgbWeights) {
 		super((s) -> logger.log("Seam carving: " + s), workingImage, rgbWeights, outWidth, outHeight);
-		coordinates = new Coordinate[inHeight][inWidth];
+		currHeight = inHeight;
+		currWidth = inWidth;
+		coordinates = new Coordinate[currHeight][currWidth];
 
-		forEach((y, x) -> coordinates[y][x] = new Coordinate(x, y));
-		grayScaled = new int[inHeight][inWidth];
-		setForEachParameters(inWidth, inHeight);
-		grayScaledImage = greyscale();
+		grayScaledMatrix = new int[currHeight][currWidth];
+		setForEachParameters(currWidth, currHeight);
+		BufferedImage grayScaledImage = greyscale();
 
 		forEach((y, x) -> {
 			coordinates[y][x] = new Coordinate(x,y);
+			grayScaledMatrix[y][x] = (new Color(grayScaledImage.getRGB(x, y))).getRed();
 		});
-
-		forEach((y, x) -> {
-			energyMatrix[y][x] = calcEnergyMatrix(x, y);
-		});
-
 	}
 
-	private double calcEnergyMatrix(int x, int y) {
-		double horizontalEnergy = 0;
-		double verticalEnergy = 0;
-		if(x == inWidth - 1){
-			horizontalEnergy = Math.abs(grayScaled[y][x] - grayScaled[y][x - 1]);
-		}else{
-			horizontalEnergy = Math.abs(grayScaled[y][x] - grayScaled[y][x + 1]);
+	private void updateDynamicPTableWithCostPixel() {
+		forEach((y, x) -> {
+			calcMinimalCostPixel(y, x);
+		});
+	}
+
+	private void calcMinimalCostPixel(int y, int x) {
+		if (currentMode == Mode.VERTICAL) {
+			calcVerticalCost(y, x);
+		} else {
+			calcHorizontalCost(y, x);
+		}
+	}
+
+	private void calcHorizontalCost(int y, int x) {
+		if (x == 0) {
+			if (y == 0 || y == this.currHeight -1) {
+				return;
+			} else {
+				this.dynamicPTable[y][x]+=  Math.abs(grayScaledMatrix[y - 1][x] - grayScaledMatrix[y + 1][x]);
+			}
+		} else {
+			this.dynamicPTable[y][x]+= calcHorizontalMinHorizontalValue(y, x);
+		}
+	}
+
+	private double calcHorizontalMinHorizontalValue(int y, int x) {
+		double mu;
+		double mh;
+		double md;
+
+		if (y == 0) {
+			mu = 255;
+			mh = dynamicPTable[y][x-1];
+			md = Math.abs(this.grayScaledMatrix[y+1][x] - this.grayScaledMatrix[y][x-1]) + dynamicPTable[y+1][x-1];
+		} else if (y == currHeight -1) {
+			md = 255;
+			mh = dynamicPTable[y][x-1];
+			mu = Math.abs(this.grayScaledMatrix[y - 1][x] - this.grayScaledMatrix[y][x-1]) + dynamicPTable[y-1][x-1];
+		} else {
+			md = Math.abs(this.grayScaledMatrix[y+1][x] - this.grayScaledMatrix[y][x-1])
+					+ Math.abs(grayScaledMatrix[y-1][x] - grayScaledMatrix[y+1][x])
+					+ dynamicPTable[y+1][x-1];
+			mu = Math.abs(this.grayScaledMatrix[y-1][x] - this.grayScaledMatrix[y][x-1])
+					+ Math.abs(grayScaledMatrix[y-1][x] - grayScaledMatrix[y+1][x])
+					+ dynamicPTable[y-1][x-1];
+			mh = Math.abs(this.grayScaledMatrix[y-1][x] - this.grayScaledMatrix[y+1][x]) + dynamicPTable[y][x-1];
 		}
 
-		if(y == inHeight -1){
-			verticalEnergy = Math.abs(grayScaled[y][x] - grayScaled[y - 1][x]);
+		return Math.min(mh, Math.min(mu, md));
+	}
+
+	private void calcVerticalCost(int y, int x) {
+		if (y == 0) {
+			if (x == 0 || x == this.currWidth -1) {
+				return;
+			} else {
+				this.dynamicPTable[y][x]+=  Math.abs(grayScaledMatrix[y][x-1] - grayScaledMatrix[y][x+1]);
+			}
+		} else {
+			this.dynamicPTable[y][x]+= calcVerticalMinVerticalValue(y, x);
+		}
+	}
+
+	private double calcVerticalMinVerticalValue(int y, int x) {
+		double ml;
+		double mv;
+		double mr;
+
+		if (x == 0) {
+			ml = 255;
+			mv = dynamicPTable[y-1][x];
+			mr = Math.abs(this.grayScaledMatrix[y][x+1] - this.grayScaledMatrix[y - 1][x]) + dynamicPTable[y-1][x+1];
+		} else if (x == currWidth -1) {
+			mr = 255;
+			mv = dynamicPTable[y-1][x];
+			ml = Math.abs(this.grayScaledMatrix[y - 1][x] - this.grayScaledMatrix[y][x-1]) + dynamicPTable[y-1][x-1];
+		} else {
+			mr = Math.abs(this.grayScaledMatrix[y][x+1] - this.grayScaledMatrix[y][x-1])
+					+ Math.abs(grayScaledMatrix[y][x+1] - grayScaledMatrix[y-1][x])
+					+ dynamicPTable[y-1][x+1];
+			ml = Math.abs(this.grayScaledMatrix[y][x + 1] - this.grayScaledMatrix[y][x-1])
+					+ Math.abs(grayScaledMatrix[y-1][x] - grayScaledMatrix[y][x-1])
+					+ dynamicPTable[y-1][x-1];
+			mv = Math.abs(this.grayScaledMatrix[y][x + 1] - this.grayScaledMatrix[y][x - 1]) + dynamicPTable[y-1][x];
+		}
+
+		return Math.min(mv, Math.min(ml, mr));
+	}
+
+	private void initDynamicPTableWithEnergyPixels() {
+		forEach((y,x) -> {
+			this.dynamicPTable[y][x] = calcEnergyPixel(x, y);
+		});
+	}
+
+	private double calcEnergyPixel(int x, int y) {
+		double horizontalEnergy = 0;
+		double verticalEnergy = 0;
+		if(x == currWidth - 1){
+			horizontalEnergy = Math.abs(grayScaledMatrix[y][x] - grayScaledMatrix[y][x - 1]);
 		}else{
-			Math.abs(grayScaled[y][x] - grayScaled[y + 1][x]);
+			horizontalEnergy = Math.abs(grayScaledMatrix[y][x] - grayScaledMatrix[y][x + 1]);
+		}
+
+		if(y == currHeight -1){
+			verticalEnergy = Math.abs(grayScaledMatrix[y][x] - grayScaledMatrix[y - 1][x]);
+		}else{
+			verticalEnergy = Math.abs(grayScaledMatrix[y][x] - grayScaledMatrix[y + 1][x]);
 		}
 
 		double ans = Math.sqrt(Math.pow(horizontalEnergy, 2) + Math.pow(verticalEnergy, 2));
@@ -83,6 +191,18 @@ public class BasicSeamsCarver extends ImageProcessor {
 				// and 'numberOfHorizontalSeamsToCarve' horizontal seams from the image.
 				// Note you must consider the 'carvingScheme' parameter in your procedure.
 				// Return the resulting image.
+		initDynamicPTableWithEnergyPixels();
+
+		if (carvingScheme == CarvingScheme.VERTICAL_HORIZONTAL) {
+			this.currentMode = Mode.VERTICAL;
+			updateDynamicPTableWithCostPixel();
+			this.currentMode = Mode.HORIZONTAL;
+			updateDynamicPTableWithCostPixel();
+		} else if (carvingScheme == CarvingScheme.HORIZONTAL_VERTICAL) {
+			updateDynamicPTableWithCostPixel(CarvingScheme.HORIZONTAL_VERTICAL);
+		} else {
+			updateDynamicPTableWithCostPixel(CarvingScheme.INTERMITTENT);
+		}
 		throw new UnimplementedMethodException("carveImage");
 	}
 	
